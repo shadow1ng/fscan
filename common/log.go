2ed5948d08dc86a48511c6c383f9e5f8cab99019
madneal <neal1991@sina.com>
2021-03-01 22:02:27 +0800

    fix typos and replace Println with Printf

--- common/log.go
+++ common/log.go
@@ -7,14 +7,14 @@ import (
 )
 
 var Results = make(chan string)
-var Woker = 0
+var Worker = 0
 var Start = true
 var LogSucTime int64
 var LogErr bool
 var LogErrTime int64
 
 func LogSuccess(result string) {
-	Woker++
+	Worker++
 	LogSucTime = time.Now().Unix()
 	if Start {
 		go SaveLog()
@@ -29,7 +29,7 @@ func SaveLog() {
 		if IsSave {
 			WriteFile(result, Outputfile)
 		}
-		Woker--
+		Worker--
 	}
 }
 
@@ -37,19 +37,19 @@ func WriteFile(result string, filename string) {
 	var text = []byte(result + "\n")
 	fl, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
-		fmt.Println("Open %s error, %v", filename, err)
+		fmt.Printf("Open %s error, %v\n", filename, err)
 		return
 	}
 	_, err = fl.Write(text)
 	fl.Close()
 	if err != nil {
-		fmt.Println("write %s error, %v", filename, err)
+		fmt.Printf("write %s error, %v\n", filename, err)
 	}
 }
 
 func WaitSave() {
 	for {
-		if Woker == 0 {
+		if Worker == 0 {
 			close(Results)
 			return
 		}
