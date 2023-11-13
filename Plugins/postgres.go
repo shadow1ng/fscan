52c680af0d9dd29b0bdea2e0a6b7fbfaaba4ba06
影舞者 <shadow1ng@qq.com>
2023-11-13 10:56:54 +0800

    Update postgres.go

--- Plugins/postgres.go
+++ Plugins/postgres.go
@@ -3,23 +3,22 @@ package Plugins
 import (
 	"database/sql"
 	"fmt"
-	"strings"
-	"time"
-
 	_ "github.com/lib/pq"
 	"github.com/shadow1ng/fscan/common"
+	"strings"
+	"time"
 )
 
-func PostgresScan(info common.HostInfo, flags common.Flags) (tmperr error) {
-	if flags.IsBrute {
+func PostgresScan(info *common.HostInfo) (tmperr error) {
+	if common.IsBrute {
 		return
 	}
 	starttime := time.Now().Unix()
 	for _, user := range common.Userdict["postgresql"] {
 		for _, pass := range common.Passwords {
 			pass = strings.Replace(pass, "{user}", string(user), -1)
-			flag, err := PostgresConn(info, user, pass, flags.Timeout)
-			if flag && err == nil {
+			flag, err := PostgresConn(info, user, pass)
+			if flag == true && err == nil {
 				return err
 			} else {
 				errlog := fmt.Sprintf("[-] psql %v:%v %v %v %v", info.Host, info.Ports, user, pass, err)
@@ -28,7 +27,7 @@ func PostgresScan(info common.HostInfo, flags common.Flags) (tmperr error) {
 				if common.CheckErrs(err) {
 					return err
 				}
-				if time.Now().Unix()-starttime > (int64(len(common.Userdict["postgresql"])*len(common.Passwords)) * flags.Timeout) {
+				if time.Now().Unix()-starttime > (int64(len(common.Userdict["postgresql"])*len(common.Passwords)) * common.Timeout) {
 					return err
 				}
 			}
@@ -37,13 +36,13 @@ func PostgresScan(info common.HostInfo, flags common.Flags) (tmperr error) {
 	return tmperr
 }
 
-func PostgresConn(info common.HostInfo, user string, pass string, timeout int64) (flag bool, err error) {
+func PostgresConn(info *common.HostInfo, user string, pass string) (flag bool, err error) {
 	flag = false
 	Host, Port, Username, Password := info.Host, info.Ports, user, pass
 	dataSourceName := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=%v", Username, Password, Host, Port, "postgres", "disable")
 	db, err := sql.Open("postgres", dataSourceName)
 	if err == nil {
-		db.SetConnMaxLifetime(time.Duration(timeout) * time.Second)
+		db.SetConnMaxLifetime(time.Duration(common.Timeout) * time.Second)
 		defer db.Close()
 		err = db.Ping()
 		if err == nil {
