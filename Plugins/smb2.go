29beca41d0f79cc4dbe618e932b42c20725afc91
影舞者 <shadow1ng@qq.com>
2023-11-13 11:24:44 +0800

    Update smb2.go

--- Plugins/smb2.go
+++ Plugins/smb2.go
@@ -2,40 +2,39 @@ package Plugins
 
 import (
 	"fmt"
+	"github.com/shadow1ng/fscan/common"
 	"net"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/shadow1ng/fscan/common"
-
 	"github.com/hirochachacha/go-smb2"
 )
 
-func SmbScan2(info common.HostInfo, flags common.Flags) (tmperr error) {
-	if flags.IsBrute {
+func SmbScan2(info *common.HostInfo) (tmperr error) {
+	if common.IsBrute {
 		return nil
 	}
 	hasprint := false
 	starttime := time.Now().Unix()
-	hash := flags.HashBytes
+	hash := common.HashBytes
 	for _, user := range common.Userdict["smb"] {
 	PASS:
 		for _, pass := range common.Passwords {
 			pass = strings.Replace(pass, "{user}", user, -1)
-			flag, err, flag2 := Smb2Con(info, flags, user, pass, hash, hasprint)
+			flag, err, flag2 := Smb2Con(info, user, pass, hash, hasprint)
 			if flag2 {
 				hasprint = true
 			}
-			if flag {
+			if flag == true {
 				var result string
-				if flags.Domain != "" {
-					result = fmt.Sprintf("[+] SMB2:%v:%v:%v\\%v ", info.Host, info.Ports, flags.Domain, user)
+				if common.Domain != "" {
+					result = fmt.Sprintf("[+] SMB2 %v:%v:%v\\%v ", info.Host, info.Ports, common.Domain, user)
 				} else {
-					result = fmt.Sprintf("[+] SMB2:%v:%v:%v ", info.Host, info.Ports, user)
+					result = fmt.Sprintf("[+] SMB2 %v:%v:%v ", info.Host, info.Ports, user)
 				}
 				if len(hash) > 0 {
-					result += "hash: " + flags.Hash
+					result += "hash: " + common.Hash
 				} else {
 					result += pass
 				}
@@ -43,8 +42,8 @@ func SmbScan2(info common.HostInfo, flags common.Flags) (tmperr error) {
 				return err
 			} else {
 				var errlog string
-				if len(flags.Hash) > 0 {
-					errlog = fmt.Sprintf("[-] smb2 %v:%v %v %v %v", info.Host, 445, user, flags.Hash, err)
+				if len(common.Hash) > 0 {
+					errlog = fmt.Sprintf("[-] smb2 %v:%v %v %v %v", info.Host, 445, user, common.Hash, err)
 				} else {
 					errlog = fmt.Sprintf("[-] smb2 %v:%v %v %v %v", info.Host, 445, user, pass, err)
 				}
@@ -54,11 +53,11 @@ func SmbScan2(info common.HostInfo, flags common.Flags) (tmperr error) {
 				if common.CheckErrs(err) {
 					return err
 				}
-				if time.Now().Unix()-starttime > (int64(len(common.Userdict["smb"])*len(common.Passwords)) * flags.Timeout) {
+				if time.Now().Unix()-starttime > (int64(len(common.Userdict["smb"])*len(common.Passwords)) * common.Timeout) {
 					return err
 				}
 			}
-			if len(flags.Hash) > 0 {
+			if len(common.Hash) > 0 {
 				break PASS
 			}
 		}
@@ -66,8 +65,8 @@ func SmbScan2(info common.HostInfo, flags common.Flags) (tmperr error) {
 	return tmperr
 }
 
-func Smb2Con(info common.HostInfo, flags common.Flags, user string, pass string, hash []byte, hasprint bool) (flag bool, err error, flag2 bool) {
-	conn, err := net.DialTimeout("tcp", info.Host+":445", time.Duration(flags.Timeout)*time.Second)
+func Smb2Con(info *common.HostInfo, user string, pass string, hash []byte, hasprint bool) (flag bool, err error, flag2 bool) {
+	conn, err := net.DialTimeout("tcp", info.Host+":445", time.Duration(common.Timeout)*time.Second)
 	defer func() {
 		if conn != nil {
 			conn.Close()
@@ -78,7 +77,7 @@ func Smb2Con(info common.HostInfo, flags common.Flags, user string, pass string,
 	}
 	initiator := smb2.NTLMInitiator{
 		User:   user,
-		Domain: flags.Domain,
+		Domain: common.Domain,
 	}
 	if len(hash) > 0 {
 		initiator.Hash = hash
@@ -100,13 +99,13 @@ func Smb2Con(info common.HostInfo, flags common.Flags, user string, pass string,
 	}
 	if !hasprint {
 		var result string
-		if flags.Domain != "" {
-			result = fmt.Sprintf("[*] SMB2-shares:%v:%v:%v\\%v ", info.Host, info.Ports, flags.Domain, user)
+		if common.Domain != "" {
+			result = fmt.Sprintf("[*] SMB2-shares:%v:%v:%v\\%v ", info.Host, info.Ports, common.Domain, user)
 		} else {
 			result = fmt.Sprintf("[*] SMB2-shares:%v:%v:%v ", info.Host, info.Ports, user)
 		}
 		if len(hash) > 0 {
-			result += "hash: " + flags.Hash
+			result += "hash: " + common.Hash
 		} else {
 			result += pass
 		}
@@ -127,5 +126,51 @@ func Smb2Con(info common.HostInfo, flags common.Flags, user string, pass string,
 	defer f.Close()
 	flag = true
 	return
+	//bs, err := ioutil.ReadAll(f)
+	//if err != nil {
+	//	return
+	//}
+	//fmt.Println(string(bs))
+	//return
 
 }
+
+//if info.Path == ""{
+//}
+//path = info.Path
+//f, err := fs.OpenFile(path, os.O_RDONLY, 0666)
+//if err != nil {
+//	return
+//}
+//flag = true
+//_, err = f.Seek(0, io.SeekStart)
+//if err != nil {
+//	return
+//}
+//bs, err := ioutil.ReadAll(f)
+//if err != nil {
+//	return
+//}
+//fmt.Println(string(bs))
+//return
+//f, err := fs.Create(`Users\Public\Videos\hello.txt`)
+//if err != nil {
+//	return
+//}
+//flag = true
+//
+//_, err = f.Write([]byte("Hello world!"))
+//if err != nil {
+//	return
+//}
+//
+//_, err = f.Seek(0, io.SeekStart)
+//if err != nil {
+//	return
+//}
+//bs, err := ioutil.ReadAll(f)
+//if err != nil {
+//	return
+//}
+//fmt.Println(string(bs))
+//return
