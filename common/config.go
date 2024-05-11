d470a91d55a45b7e3189dd275d82cfff03fbc9b4
shadow1ng <shadow1ng@qq.com>
2024-05-11 16:09:14 +0800

    优化报错处理

--- common/config.go
+++ common/config.go
@@ -1,6 +1,6 @@
 package common
 
-var version = "1.8.3"
+var version = "1.8.4"
 var Userdict = map[string][]string{
 	"ftp":        {"ftp", "admin", "www", "web", "root", "db", "wwwroot", "data"},
 	"mysql":      {"root", "mysql"},
